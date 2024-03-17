aa141a1a8d3a752fdc63c2c4eaa098cdf7afbb72
Franciszek Pietrzycki <247762@edu.p.lodz.pl>
2024-03-17 22:10:48 +0100

    Extreme multiplying tests

--- CalculatorLibraryTest/UnitTest1.cs
+++ CalculatorLibraryTest/UnitTest1.cs
@@ -10,7 +10,35 @@ namespace CalculatorLibraryTest
         public void isMultiplyingOK()
         {
             CalculatorLibrary.MultiplyingCalculator mcalc = new CalculatorLibrary.MultiplyingCalculator();
-            Assert.IsTrue(true);
+            int result = mcalc.multiply(3, 5);
+            Assert.AreEqual(15, result);
+            result = mcalc.multiply(-3, -5);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void MultiplyingBy0()
+        {
+            CalculatorLibrary.MultiplyingCalculator mcalc = new CalculatorLibrary.MultiplyingCalculator();
+            int result = mcalc.multiply(11, 0);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void ExtremeMultiplying()
+        {
+            CalculatorLibrary.MultiplyingCalculator mcalc = new CalculatorLibrary.MultiplyingCalculator();
+            int result = mcalc.multiply(int.MaxValue, 0);
+            Assert.AreEqual(0, result);
+
+            result = mcalc.multiply(int.MinValue, 0);
+            Assert.AreEqual(0, result);
+
+            result = mcalc.multiply(int.MinValue, int.MaxValue);
+            Assert.AreEqual(int.MinValue, result);
+
+            result = mcalc.multiply(int.MinValue, -1);
+            Assert.AreEqual(int.MinValue, result);
         }
     }
 }
