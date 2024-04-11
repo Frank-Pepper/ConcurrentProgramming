c9f8d71409f02bc3587285bca1108e0daabe05db
Frank <reppepf@gmail.com>
2024-04-11 21:39:38 +0200

    They see me ballin

---
+++ ConcurrentProgramming/Logic/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Data;
+
+namespace Logic
+{
+    internal class Ball : IBall
+    {
+        private float x;
+        private float y;
+        private float vx;
+        private float vy;
+        public Ball(float width, float height)
+        {
+            x = Random.NextDouble() * width;
+            y = Random.NextDouble() * height;
+            
+        }
+
+        public void Move()
+        {
+            x += vx;
+            y += vy;
+        }
+    }
+}
