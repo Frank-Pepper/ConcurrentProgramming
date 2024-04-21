cbd561db295c484a5f13d1416202603b56a5ff2e
kjasamil <kamil200202@gmail.com>
2024-04-21 12:08:38 +0200

    Logic test fix

--- LogicTest/BallManagerTest.cs
+++ LogicTest/BallManagerTest.cs
@@ -13,7 +13,11 @@ namespace LogicTest
             var TestBallRepository = Substitute.For<Data.IBallRepository>();
             TestBallRepository.When(x => x.Add(Arg.Any<Data.IBall>())).Do(x => i++);
             var TestBallManager = ILogicAPI.GetBallManager(TestBallRepository);
-            var balls = new List<ILogicBallEvent> (new ILogicBallEvent[5]);
+            var balls = new List<ILogicBallEvent>();
+            for(int j = 0; j < 5; j++)
+            {
+                balls.Add(ILogicAPI.GetLogicBallEventSubOnly(null));
+            }
             TestBallManager.Create(5, 100, 100, balls);
             Assert.AreEqual(5, i);
         }
